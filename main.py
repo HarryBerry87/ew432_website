35e21199e278cab9771458b3ad2d6a9d7caaf0dd
HarryBerry87 <46969575+HarryBerry87@users.noreply.github.com>
2019-03-07 11:04:05 -0500

    all pages

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/album")
+def album():
+    return render_template('album.html')
